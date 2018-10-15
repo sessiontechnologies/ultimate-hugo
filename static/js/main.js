290df3a56a5e93627920f1715cb93970724da5df
Maurizio Pillitu <maoo@apache.org>
2018-10-15 12:26:02 +0200

    :tada: major cleanup

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,20 @@
 $(document).ready(function(){
 	$('#clients-logo').owlCarousel({
-	    autoPlay: 3000,
+	    autoplay: true,
+	    rewind: true,
+	    autoplayTimeout:3000,
+		autoplayHoverPause:false,
 		items : 5,
 		nav: false,
-		dots: false,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
+		dots: false
 	});
 
-	// $("#testimonial-slider").owlCarousel({
-	//     paginationSpeed : 500,      
-	//     singleItem:true,
-	//     autoPlay: 3000,
-	// });
-
-	// $("#clients-logo").owlCarousel({
-	// 	autoPlay: 3000,
-	// 	items : 5,
-	// 	itemsDesktop : [1199,5],
-	// 	itemsDesktopSmall : [979,5],
-	// });
-
-	// $("#works-logo").owlCarousel({
-	// 	autoPlay: 3000,
-	// 	items : 5,
-	// 	itemsDesktop : [1199,5],
-	// 	itemsDesktopSmall : [979,5],
-	// });
-
-	// google map
-	// function initMap() {
-	//   var map = new google.maps.Map(document.getElementById('map'), {
-	//     center: {lat: -34.397, lng: 150.644},
-	//     zoom: 8
-	//   });
-	// }
-
+	// TODO - Find alternative
 	// Counter
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+	// $('.counter').counterUp({
+	//        delay: 10,
+	//        time: 1000
+	//    });
 });
 
 
